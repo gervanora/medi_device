35432b05b1e271875e893405d64ad8c49be6ed18
arisen <arisengeorge@gmail.com>
2017-03-20 23:07:02 +0530

    initial commit

---
+++ addproduct_form.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Medical Device Database System</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+	 	<div class="header">Medical Device Database System</div>
+		<div class="content">
+			
+		</div>
+		<div class="footer">Copyright @2017</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Medical Device Database System</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+	 	<div class="header">Medical Device Database System</div>
+		<div class="content">
+			<a href="addproduct_form.php">Add Products</a>
+			<a href="#">List Companies</a>
+			<a href="#">List Brands</a>
+			<a href="#">Product Classifications</a>
+		</div>
+		<div class="footer">Copyright @2017</div>
+	</body>
+</html>
