4ecb0366089094a475415841d7ead19e4410ffe8
arisen <arisengeorge@gmail.com>
2017-05-26 00:34:34 +0530

    search

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include 'includes/header.php'; ?>
 <?php
+
+    $authors_query = "Select * FROM users";
+    $authors_list = $conn->query($authors_query);
+
 	if(!empty($_POST)){
 
         $conditions = array();
@@ -12,6 +16,29 @@
 		if(!empty($_POST['company_name'])){
 			$conditions[] = "company_name  LIKE '%".$_POST['company_name']."%'";
 		}
+
+		if(!empty($_POST['author'])){
+			$conditions[] = "products.user_id = ".$_POST['author']." ";
+		}
+
+		//created to and from
+
+        if(!empty($_POST['product_type'])){
+			//$conditions[] = "products.user_id = ".$_POST['author']." ";
+			if($_POST['product_type'] == "stub")
+			{
+				$conditions[] = "products.profile_status = 'Stub'";
+			}
+			elseif ($_POST['product_type'] == "pipeline") {
+				$conditions[] = "products.pipelined_marketed = 'Pipeline'";
+			}
+			else{
+				$conditions[] = "products.pipelined_marketed = 'Markets'";
+			}
+		}
+
+
+
         echo "<pre>";
 		print_r($conditions);
 		echo "</pre>";
@@ -19,7 +46,7 @@
 		if(!empty($conditions)){
 			$sql = $sql." WHERE ".implode(" AND ", $conditions);
 		}
-		echo $sql;
+		//echo $sql;
 		$result = $conn->query($sql);
 	}
 	else{
@@ -28,33 +55,119 @@
 			$result = $conn->query($sql);
 	}
 ?>
-		<div class="content">
+		<div  class="content" align="left">
 			<h3>Search Form</h3>
 			<form method="POST" action="">
-				<div><label>Product Name</label><input type="text" name="product_name"/></div>
-                <div><label>Company Name</label><input type="text" name="company_name"/></div>
-                <div><label>Author</label><input type="text" name="author"/></div>
-                <div>Created Date:<input type="date"  name="created_from"> To <input type="date" name="created_to"></div>
-                <div>
-                	pipeline<input type="radio" value="pipeline">
-                	marketed<input type="radio" value="marketed">
-                	stub<input type="radio" value="stub">
-                </div>
-                <div>
-                	<label>Technology</label>
-                	<select>
-                		<option></option>
+<table class="div_label">
+ <tr class="div_label">
+    <td width=25%>
+	
+		<div>
+			<label>Product Name</label>
+		</div>
+	</td>
+    <td width=25%>	
+		<div>
+			<input type="text" name="product_name" id="product_name"/>
+		</div>
+	</td >
+	<td width=25%> &nbsp;</td>
+	<td width=25%> &nbsp;</td>
+ </tr>
+ <tr class="div_label">
+		<td>
+	
+                <div ><label>Company Name</label></div>
+		</td>
+		<td width=25%>	
+				<div><input type="text" name="company_name" id="company_name"/></div>
+		</td>		
+		<td width=25%> &nbsp;</td>
+	<td width=25%> &nbsp;</td>
+ </tr>
+ <tr class="div_label">
+    <td width=25%>
+		
+                <div ><label>Author</label></div>
+    </td>
+    <td width=25%>
+				<select name="author">
+                	        <option value=""></option>
+                		<?php while($row = $authors_list->fetch_assoc()) { ?>
+                			<option value="<?php echo $row['id']; ?>"><?php echo $row['username']?></option>
+                		<?php } ?>
                 	</select>
-                </div>
-                <div>
-                	<label>Market Level</label>
-                	<select>
-                		<option></option>
-                	</select>
-                </div>
-                <div><label>510K Number</label><input type="text" name="510_num"/></div>
-                <div><label>PMA Number</label><input type="text" name="pma_num"/></div>
-                <input type="submit" value="Search">
+		</td>	
+	<td width=25%> &nbsp;</td>
+	<td width=25%> &nbsp;</td>
+ </tr>	
+
+ <tr class="div_label">
+        <td colspan=2 width=50%>
+               <div><label>Created Date:	</label>
+<input type="date"  name="created_from">
+</div>
+		
+	</td>
+	<td colspan=2>To  <input type="date" name="created_to"></td>
+		
+ </tr>
+ <tr class="div_label">
+		<td width=25%>                
+			<div>
+                		pipeline<input name="product_type" type="radio" value="pipeline">
+			</div>
+		</td>
+		<td width=25%>  
+		 	<div>
+                		marketed<input name="product_type" type="radio" value="marketed">
+			</div>
+		</td>
+		<td width=25%>  
+			<div>
+                		stub<input name="product_type" type="radio" value="stub">
+
+                	</div>
+		</td>
+	<td width=25%> &nbsp;</td>
+ </tr>
+
+  <tr class="div_label">
+    <td width=25%>
+            <label>Technology</label>
+
+	</td>
+	<td width=25%>
+            <input type="text" name="product_technology" id="product_technology">
+                
+	</td>
+</tr>
+<tr class="div_label">
+	<td width=25%>
+    	<label>Market Level</label>
+	</td>
+	<td>
+		<input type="text" name="market_level" id="market_level">
+	</td>
+</tr>
+
+  <tr class="div_label">
+
+	<td width=25%>
+                <div><label>510K Number</label></div></td>
+	<td width=25%><div><input type="text" name="510_num"/></div>
+     </td>
+     <td width=25%> &nbsp;</td> 
+<tr class="div_label">
+	<td width=25%><div><label>PMA Number</label></div></td>
+	<td width=25%><div><input type="text" name="pma_num"/></div></td>
+</tr>
+<tr>
+        <td width=25%><input type="submit" value="Search"></td>
+				
+				
+</tr>
+</table>
 			</form>
 		</div>
 
@@ -106,4 +219,24 @@
 		</table>
 	  	</div>
 		</div>
+<script type="text/javascript">
+	$('document').ready(function(){
+		$("#company_name").autocomplete({
+                source: 'company_search.php'
+            });
+
+		$("#product_name").autocomplete({
+                source: 'brand_search.php'
+            });
+
+		$("#product_technology").autocomplete({
+            source: 'technology_search.php',        
+        }); 
+
+        $("#market_level").autocomplete({
+	          source: 'search_classification.php',
+	      });
+
+	});
+</script>
 <?php include 'includes/footer.php'; ?>
